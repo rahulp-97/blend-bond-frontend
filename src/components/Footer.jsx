aa9f090d051dced11ce0d7e363c2c13b0222c5be
Jayan Patel <rahul159147@gmail.com>
2025-09-28 12:58:29 +0530

    footer company name fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer style={{marginTop: '100px', position: 'relative'}} className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by matchWith app</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Blend bond app</p>
   </aside>
 </footer>
   );
