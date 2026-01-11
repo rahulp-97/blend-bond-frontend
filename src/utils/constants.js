2fc0d3234844b6df7bfae512924636971723f2d9
Rahul Prajapati <rahul159147@gmail.com>
2026-01-11 15:05:11 +0530

    frontend deployment on test env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_PRODUCTION_URL : "http://localhost:8000";
 // export const BASE_URL = '/api';
